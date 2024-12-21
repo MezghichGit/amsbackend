b3a52411e6290ee6a128b262ff25f013b980e1e4
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-12-21 18:42:42 +0100

    crud complet Provider

--- src/main/java/com/sip/controllers/ProviderRestController.java
+++ src/main/java/com/sip/controllers/ProviderRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.sip.services.ProviderService;
 
 @RestController
 @RequestMapping({ "/providers" })
+@CrossOrigin(origins = "*")
 public class ProviderRestController {
 
 	@Autowired
